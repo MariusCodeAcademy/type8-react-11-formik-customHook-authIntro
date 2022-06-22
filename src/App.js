236d0c0e09862df86cc507dc0cab7243467dd525
Marius <marius@example.com>
2022-06-22 10:06:58 +0300

    localstorageSupport

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import { Route, Switch } from 'react-router';
-import { Link } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
-import LoginForm from './components/LoginForm/LoginForm';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import AboutPage from './pages/About/AboutPage';
 import LoginPage from './pages/LoginPage/LoginPage';
-import { useAuthCtx } from './store/authContext';
 
 function App() {
-  const { isUserLoggedIn } = useAuthCtx();
-
   // 1. gauti isUserLoggedIn ir priklausomai nuo jo generuoti arba ne /about
   // 2. priklausomai nuio isUserLoggedIn generuoti arba about page arba pranesta kad sis puslapis tik registruotiems vartotojams
   // 3. Sukurti <NotLoggedIn /> komponenta kuri rodom jei useris neturi teises matyti psl
@@ -19,17 +14,9 @@ function App() {
       <Header />
       <Switch>
         {/* negeneruoti route /about jei nesam prisilogine */}
-        <Route path='/about'>
-          {isUserLoggedIn ? (
-            <AboutPage />
-          ) : (
-            <>
-              <h2>Please login</h2>
-              {/* <Link to={'/login'}>Login here</Link> */}
-              <LoginForm />
-            </>
-          )}
-        </Route>
+        <ProtectedRoute path='/about'>
+          <AboutPage />
+        </ProtectedRoute>
         <ProtectedRoute path='/contact'>
           <h2>contact</h2>
         </ProtectedRoute>
